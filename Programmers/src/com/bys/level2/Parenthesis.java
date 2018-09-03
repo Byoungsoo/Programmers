9f228c4e25738c2a7c793de58e49b74514aa1941
byoungsoo.ko <byoungsoo.ko@HLBRD10-NE1033J.LGE.NET>
2018-09-03 18:00:13 +0900

    TT

---
+++ Programmers/src/com/bys/level2/Parenthesis.java
@@ -0,0 +1,27 @@
+package com.bys.level2;
+
+public class Parenthesis {
+	
+	
+	boolean solution(String s) {
+        boolean answer = true;
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        
+        
+        
+        
+        System.out.println(answer);
+
+        return answer;
+    }
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "(())";
+		Parenthesis p = new Parenthesis();
+		p.solution(str);
+		
+	}
+
+}
