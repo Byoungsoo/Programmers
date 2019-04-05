510ab4c4560edc92dfd51cd2fe367a1a58fa14d1
ByoungsooKo <skwltg90@naver.com>
2019-04-05 13:31:25 +0900

    1

--- Programmers/src/com/bys/solution/Solution.java
+++ Programmers/src/com/bys/solution/Solution.java
@@ -4,12 +4,11 @@ class Solution {
 
 	public int solution(String numbers) {
 		int answer = 0;
+		boolean flag = false; 
 		char[] number = new char[numbers.length()];
 		for (int i = 0; i < numbers.length(); i++) {
 			number[i] = numbers.charAt(i);
 		}
-		
-		
 		
 		
 		return answer;
@@ -39,7 +38,7 @@ class Solution {
 		Solution s = new Solution();
 
 		numbers = "123";
-		s.solution(numbers);
+		System.out.println(s.solution(numbers));
 
 	}
 
